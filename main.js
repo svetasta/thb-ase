d8cc375e24f330374512d1d8ed6e97fe56b59cf8
explit <s75788@beuth-hochschule.de>
2023-11-20 00:59:13 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const questions = require('./questions');
+const answersApp = require('./answers');
+
+// Use the questions as needed
+console.log('Questions:', questions);
+
+// Use the answers route handler directly at the root URL '/'
+app.use('/', answersApp);
+
+// Start the server
+const server = app.listen(8080, function () {
+  console.log("Server runs on port 8080");
+});
