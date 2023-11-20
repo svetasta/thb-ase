1b95804c03563d068a379123f5f489fa1fef9716
explit <s75788@beuth-hochschule.de>
2023-11-20 02:09:39 +0100

    main crrected

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const questions = require('./answers');
+
 const answersApp = require('./answered_questions');
 
 // Use the questions as needed
 //console.log('Questions:', questions);
 
 // Use the answers route handler directly at the root URL '/'
-app.use('/', answersApp);
+app.use('/', answersApp.app);
 
 // Start the server
 const server = app.listen(8080, function () {
