301b9426236a2066e10c7a9e37cd65dfd1ab5828
explit <s75788@beuth-hochschule.de>
2023-11-20 01:49:45 +0100

    answers.js added

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const questions = require('./questions');
-const answersApp = require('./answers');
+//const questions = require('./questions');
+const answersApp = require('./answered_questions');
 
 // Use the questions as needed
-console.log('Questions:', questions);
+//console.log('Questions:', questions);
 
 // Use the answers route handler directly at the root URL '/'
 app.use('/', answersApp);
