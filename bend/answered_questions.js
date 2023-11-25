90bda73c5c0bce0cf551d0c8a1fa1003a3806787
explit <s75788@beuth-hochschule.de>
2023-11-25 21:06:40 +0100

    changed array

--- bend/answered_questions.js
+++ bend/answered_questions.js
@@ -1,14 +1,12 @@
 const  express = require ('express');
 const  app = express();
 const { v4: uuidv4 } = require('uuid');
-const answeredQuestions = [
-
-	{ id: 1, text: 'Wie richte ich das WLAN ein? ', answer:'Für Gäste muss der Antrag auf WLAN mindestens einen Tag vor der Nutzung eingereicht werden. (Bitte wenden Sie sich hierzu an die IT-Administratoren in Ihrem Bereich.) Mitglieder akademischer Einrichtungen haben kostenlosen Zugang zum eduroam WLAN. Eine Anleitung zur Nutzung finden Sie unter folgendem Link.https://it.th-brandenburg.de/it-dienste/dienste-rz/wlan/'},
-	{ id: 2, text: 'string2', answer:''},
-	{ id: 3, text: 'string3', answer:''},
-	{ id: 4, text: 'string4', answer:''},
-	{ id: 5, text: 'string5', answer:''},
-	{ id: 6, text: 'string6', answer:''}
+const answeredQuestions = [{id: 1, text: 'Wie richte ich das WLAN ein? ', answer:'Für Gäste muss der Antrag auf WLAN mindestens einen Tag vor der Nutzung eingereicht werden. (Bitte wenden Sie sich hierzu an die IT-Administratoren in Ihrem Bereich.) Mitglieder akademischer Einrichtungen haben kostenlosen Zugang zum eduroam WLAN. Eine Anleitung zur Nutzung finden Sie unter folgendem Link.https://it.th-brandenburg.de/it-dienste/dienste-rz/wlan/'},
+	{id: 2, text: 'string2', answer:''},
+	{id: 3, text: 'string3', answer:''},
+	{id: 4, text: 'string4', answer:''},
+	{id: 5, text: 'string5', answer:''},
+	{id: 6, text: 'string6', answer:''}
   ];
   
 
